4babbaf17b979e9875e184de0555ab236b2cef9c
Steven Fiero <ssfiero@yahoo.com>
2017-11-05 23:11:41 -0600

    Checkerboard Commplete - WIP colors and gradient

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Your JS goes here
+function makeBoard() {
+
+  // 9 column x 7 rows = 63 total divs
+  for (let i = 0; i < 63; i++) {
+    let body = document.querySelector('body');
+    let checkDiv = document.createElement('div')
+    let checker = body.appendChild(checkDiv);
+
+    if (i % 2 === 0) {
+      checker.setAttribute("style", "width: 11.1%; float: left; paddingBottom: 11.1%; height: 100px; background-color: red");
+    } else {
+      checker.setAttribute("style", "width: 11.1%; float: left; paddingBottom: 11.1%; height: 100px; background-color: black");
+    }
+  }
+
+};
+
+makeBoard();
